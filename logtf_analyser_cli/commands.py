852cf492d9ea83ca83013f2b94bf640852ff7a81
cob16 <cormac.brady@hotmail.co.uk>
2017-10-10 16:00:24 +0100

    Safe rollbacks on exceptions and user prompts subcommand name refactor

--- logtf_analyser_cli/commands.py
+++ logtf_analyser_cli/commands.py
@@ -30,21 +30,29 @@ def download(userid: 'Steam User Id64', limit: 'Number or logs to get' = 5):
         exit(2)
 
     logging.info(F"Querying latest {limit} logs for user {userid} from logs.tf...")
-    logs = search_logs(player=userid, limit=limit)
-    logging.info(F"Got {len(logs)} results")
-    logs = LogSearch().db_load(logs)
-    logging.info(F"{len(logs.existing_logs)} existing logs and {len(logs.newLogs)} new logs")
-    if logs.newLogs:
-        prompt_options = [
-            {'selector': 'y', 'prompt': 'Yes, to download all new logs', 'return': True},
-            {'selector': 'n', 'prompt': 'No, and exit program', 'return': False}
-        ]
-        if prompt.options(colored.magenta(
-                        F"Download {len(logs.newLogs)} logs? \nThis will download aprox {round(len(logs.newLogs) * LOG_SIZE_KB, 3)}KB of data " +
-                        F"and commit aprox {round(len(logs.newLogs) * DB_INCREASE_KB, 3)}KB to DB",
-                bold=True), prompt_options):
-            download_chat_logs(logs.newLogs, parent['ignore_console'])
-            logging.info(colored.green("Successfully downloaded all logs!"))
+    with db.atomic():
+        with db.savepoint() as save:
+            logs = search_logs(player=userid, limit=limit)
+            logging.info(F"Got {len(logs)} results")
+            logs = LogSearch().db_load(logs)
+            logging.info(F"{len(logs.existing_logs)} existing logs and {len(logs.newLogs)} new logs")
+            if logs.newLogs:
+                if download_prompt(len(logs.newLogs)):
+                    download_chat_logs(logs.newLogs, parent['ignore_console'])
+                    logging.info(colored.green("Successfully downloaded all logs!"))
+                else:
+                    save.rollback()
+
+
+def download_prompt(num_new_logs: int):
+    prompt_options = [
+        {'selector': 'y', 'prompt': 'Yes, to download all new logs', 'return': True},
+        {'selector': 'n', 'prompt': 'No, and exit program', 'return': False}
+    ]
+    prompt_msg = F"Download {num_new_logs} logs? " \
+                 F"\nThis will download aprox {round(num_new_logs * LOG_SIZE_KB, 3)}" \
+                 F"KB of data and commit aprox {round(num_new_logs * DB_INCREASE_KB, 3)}KB to DB "
+    return prompt.options(colored.magenta(prompt_msg, bold=True), prompt_options)
 
 
 def download_chat_logs(logs, ignore_console):
@@ -64,18 +72,22 @@ def count():
 
 @begin.subcommand
 def prune():
+    deleted_rows = _prune_query().execute()
+    logging.info(colored.red(F"Deleted {deleted_rows} logs"))
+
+
+def _prune_query():
     query = Log.delete().where(
         Log.log_id.not_in(
             Chat.select(Chat.log)
         )
     )
-    deleted_rows = query.execute()
-    logging.info(colored.red(F"Deleted {deleted_rows} logs"))
+    return query
 
 
 @begin.subcommand
 @begin.convert(steam_id=int, search_str=str, count_only=bool)
-def show(steam_id=None, search_str=None, count_only: "get only count of results" = False):
+def chat(steam_id=None, search_str=None, count_only: "get only count of results" = False):
     query = Chat.select(Log.log_id, Log.date, Log.title, Chat.msg, Chat.username).join(Log)
 
     if steam_id:
